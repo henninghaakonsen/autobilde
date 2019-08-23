d775aab80d1d3f8a6d3a69392eb2cb230acc7bd1
Henning Håkonsen <henninghaakonsen@me.com>
2019-08-23 21:15:50 +0200

    Fjerner express kall.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const { setupVær } = require("./src/backend/vær");
 const port = process.env.PORT || 9000;
 
 app.use(express.static(path.join(__dirname, "build")));
@@ -9,8 +8,6 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-
-setupVær(app);
 
 app.listen(port, () => {
     console.log(`Started server on ` + port);
